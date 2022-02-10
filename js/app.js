f247f153d357f52d13ccbd425b184cde9ef9be62
Mohsin Ansari <ansarimohsin667@gmail.com>
2022-02-11 01:52:22 +0530

    updating files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
-$(".nice").niceScroll({
-  cursorcolor: "#3457D1",
-  cursorwidth: "4px"
+$(document).ready(() => {
+  $(".nice").niceScroll({
+    cursorcolor: "#3457D1",
+    cursorwidth: "4px"
+  });
+
+  $("#close").on("click", function () {
+    $(".dashborad-navbar").addClass("slide-out");
+
+    $(".sidebar.side").addClass("slide-out");
+  });
+
+  $("#open").on("click", function () {
+    $(".dashborad-navbar").removeClass("slide-out");
+    $(".sidebar.side").removeClass("slide-out");
+  });
+
+  // $("#datetimepicker1").data("DateTimePicker").show();
+
+  // <div class="datepicker"><input type="text" autocomplete="off" name="date" id="date" placeholder="no"></input>
+  // $("#date").datepicker({
+  //   changeMonth: true,
+  //   changeYear: true,
+  //   dateFormat: "dd/mm/yy",
+  //   yearRange: "-90:+00"
+  // });
+  // $("#date").datepicker("show");
 });
