fdee77184a161021be23cbd8c285ae6fa2acb383
Mohsin Ansari <ansarimohsin667@gmail.com>
2022-02-12 02:08:36 +0530

    working on my event screen

--- js/app.js
+++ js/app.js
@@ -18,11 +18,11 @@ $(document).ready(() => {
   // $("#datetimepicker1").data("DateTimePicker").show();
 
   // <div class="datepicker"><input type="text" autocomplete="off" name="date" id="date" placeholder="no"></input>
-  // $("#date").datepicker({
-  //   changeMonth: true,
-  //   changeYear: true,
-  //   dateFormat: "dd/mm/yy",
-  //   yearRange: "-90:+00"
-  // });
-  // $("#date").datepicker("show");
+  $("#date").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    dateFormat: "dd/mm/yy",
+    yearRange: "-90:+00"
+  });
+  $("#date").datepicker("show");
 });
