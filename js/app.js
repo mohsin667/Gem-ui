487d2c2db3e0aeab65504468779dd8de83c95d6e
Mohsin Ansari <ansarimohsin667@gmail.com>
2022-02-12 23:36:33 +0530

    working on my event list page

--- js/app.js
+++ js/app.js
@@ -26,3 +26,21 @@ $(document).ready(() => {
   });
   $("#date").datepicker("show");
 });
+
+var User = function (firstName, courseCount) {
+  this.firstName = firstName;
+  this.courseCount = courseCount;
+  this.getCourseCount = function () {
+    console.log(`Course count is: ${this.courseCount}`);
+  };
+};
+
+User.prototype.getFirstName = function () {
+  console.log(`Your first name is ${this.firstName}`);
+};
+
+var hitesh = new User("hitesh", 2);
+hitesh.getFirstName();
+console.log(hitesh);
+var sam = new User("sam", 1);
+console.log(sam);
